4a3e22d8c9c014007013d764d81463633058527e
Andrey <inokatre08@gmail.com>
2026-05-07 22:38:21 +0300

    fixed some mistakes1

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/Anvrey/React_inputs_test/'
+  base: '/React_inputs_test/',
 })
