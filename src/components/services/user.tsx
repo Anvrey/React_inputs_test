115efdee9ffea1993c0a496a25602bd32448e51a
Andrey <inokatre08@gmail.com>
2026-05-07 22:30:54 +0300

    fixed some mistakes

--- src/components/services/user.tsx
+++ src/components/services/user.tsx
@@ -2,5 +2,5 @@ import { User } from "../types/User";
 import usersFromServer from '../api/users.json';
 
 export function getUserById(userId: number): User | null {
-  return usersFromServer.find(user => user.id === userId);
+  return usersFromServer.find(user => user.id === userId) || null;
 };
